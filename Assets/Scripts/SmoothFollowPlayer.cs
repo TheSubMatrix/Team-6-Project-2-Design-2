70204ea9d4995d2d2728539a694e03046e62a83d
Colin Whiteford <colinwhiteford516@gmail.com>
2024-09-24 20:33:35 -0400

    Organized URP assets and added a smooth camera follow

---
+++ Assets/Scripts/SmoothFollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowPlayer : MonoBehaviour
+{
+    [SerializeField] Vector3 positionOffset;
+    [SerializeField] Transform pivotToFollow;
+    [SerializeField] float followSpeed = .125f;
+
+    Vector3 velocity = new Vector3(0,0,0);
+    /// <summary>
+    /// LateUpdate is called every frame, if the Behaviour is enabled.
+    /// It is called after all Update functions have been called.
+    /// </summary>
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = pivotToFollow.position + positionOffset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, followSpeed);
+        transform.position = smoothedPosition;
+    }
+}
