376a5b667e7a8625a985ea9533442b92c2b87446
Colin Whiteford <colinwhiteford516@gmail.com>
2024-09-25 01:26:32 -0400

    Added specular highlight to toon shader

--- Assets/VFX/ToonLighting.hlsl
+++ Assets/VFX/ToonLighting.hlsl
@@ -4,18 +4,29 @@
 struct ToonLightingData
 {
     float3 normalWS;
+    float3 viewDirectionWS;
+    float4 specularColor;
+    float4 ambientColor;
     UnityTexture2D rampTexture;
     UnitySamplerState sampler_rampTexture;
     float3 albedo;
+    float smoothness;
 };
 #ifndef SHADERGRAPH_PREVIEW
+float GetSmoothnessPower(float rawSmoothness)
+{
+    return exp2(10* rawSmoothness + 1);
+}
+
 float3 ToonLightHandling(ToonLightingData toonLightingData, Light light)
 {
-    float3 radiance = light.color;
     float diffuse = dot(toonLightingData.normalWS, light.direction);
     float2 rampUV = float2(1 - (diffuse * 0.5 + 0.5), 0.5);
-    float4 rampValue = SAMPLE_TEXTURE2D(toonLightingData.rampTexture, toonLightingData.sampler_rampTexture, rampUV);
-    return toonLightingData.albedo * radiance * rampValue.r;
+    float lightIntensity = SAMPLE_TEXTURE2D(toonLightingData.rampTexture, toonLightingData.sampler_rampTexture, rampUV).r;
+    float3 radiance = light.color * lightIntensity;
+    float specularDot = saturate(dot(toonLightingData.normalWS, normalize(light.direction + toonLightingData.viewDirectionWS)));
+    float specular = smoothstep(0.005, 0.01, pow(specularDot, GetSmoothnessPower(toonLightingData.smoothness)) * diffuse);
+    return toonLightingData.albedo * (toonLightingData.ambientColor + radiance + (specular * toonLightingData.specularColor));
     
 }
 #endif
@@ -36,13 +47,17 @@ float3 CalculateToonLighting(ToonLightingData toonLightingData)
 }
 
 //Wrapper for use in shader graph
-void CalculateToonLighting_float(float3 albedo, float3 worldspaceNormal, UnityTexture2D rampTexture, UnitySamplerState sampler_rampTexture, out float3 finalCol)
+void CalculateToonLighting_float(float3 albedo, float smoothness, float4 specularColor, float4 ambientColor, float3 worldspaceNormal, float3 viewDirection, UnityTexture2D rampTexture, UnitySamplerState sampler_rampTexture, out float3 finalCol)
 {
     ToonLightingData toonLightingData;
     toonLightingData.albedo = albedo;
+    toonLightingData.specularColor = specularColor;
     toonLightingData.normalWS = worldspaceNormal;
+    toonLightingData.viewDirectionWS = viewDirection;
     toonLightingData.rampTexture = rampTexture;
     toonLightingData.sampler_rampTexture = sampler_rampTexture;
+    toonLightingData.smoothness = smoothness;
+    toonLightingData.ambientColor = ambientColor;
     finalCol = CalculateToonLighting(toonLightingData);
 }
 #endif
