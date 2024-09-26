d491cb4f290b976f8df7df1ac708403b0b39e03c
Colin Whiteford <colinwhiteford516@gmail.com>
2024-09-26 15:33:22 -0400

    Simple scene transitioner

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance { get; private set; }
+    Image blackoutImage;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this);
+        } 
+        else 
+        { 
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            blackoutImage = GetComponentInChildren<Image>();
+            StartCoroutine(Fade(0));
+        } 
+    }
+
+    IEnumerator FadeTransition(string sceneToTransitionTo, float timeForFadeIn = 0.5f, float delayForFadeIn = 0f, float timeForFadeOut = 0.5f, float delayForFadeOut = 0f)
+    {
+        yield return Fade(1, timeForFadeIn, delayForFadeIn);
+        SceneManager.LoadScene(sceneToTransitionTo);
+        yield return Fade(0, timeForFadeOut, delayForFadeOut);
+    }
+    IEnumerator Fade(float desiredAlpha, float timeForFade = 0.5f, float delay = 0f)
+    {
+        float elapsedTime = 0;
+        float startingAlpha = blackoutImage.color.a;
+        yield return new WaitForSeconds(delay);
+        while (elapsedTime <= timeForFade)
+        {
+            blackoutImage.color = new Color(blackoutImage.color.r, blackoutImage.color.b, blackoutImage.color.b, Mathf.Lerp(startingAlpha, desiredAlpha, elapsedTime / timeForFade));
+            elapsedTime += Time.deltaTime;
+            yield return null; 
+        }
+    }
+
+}
