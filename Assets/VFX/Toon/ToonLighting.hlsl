cdfc219b9c6f84e7069f734db8cc05af2c26c361
Colin Whiteford <colinwhiteford516@gmail.com>
2024-10-07 22:54:46 -0400

    Fixed flynn's materials

--- Assets/VFX/Toon/ToonLighting.hlsl
+++ Assets/VFX/Toon/ToonLighting.hlsl
@@ -31,7 +31,7 @@ float3 ToonGlobalIllumination(ToonLightingData toonLightingData)
     float3 reflectionVector = reflect(-toonLightingData.viewDirectionWS, toonLightingData.normalWS);
     float fresnel = Pow4(1- saturate(dot(normalize(toonLightingData.viewDirectionWS), normalize(toonLightingData.normalWS))));
     float rampedFresnel = Quantize(fresnel, 2);
-    float3 indirectSpecular = (GlossyEnvironmentReflection(reflectionVector, RoughnessToPerceptualRoughness(1-toonLightingData.smoothness), toonLightingData.ambientOcclusion) * rampedFresnel);
+    float3 indirectSpecular = (GlossyEnvironmentReflection(reflectionVector, RoughnessToPerceptualRoughness(1-toonLightingData.smoothness), toonLightingData.ambientOcclusion) * rampedFresnel) / 5;
     return indirectDiffuse + indirectSpecular;
 }
 float GetSmoothnessPower(float rawSmoothness)
